29297a2d446d96f0807dd87bcd1d600baa30041f
ravi sharma <ravisharma7414@gmail.com>
2023-12-15 12:17:58 +0530

    update

--- src/auth/dto/userprofile.dto.ts
+++ src/auth/dto/userprofile.dto.ts
@@ -1,4 +1,3 @@
-import { Prop } from '@nestjs/mongoose';
 import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
--- src/auth/services/profile.service.ts
+++
@@ -1,4 +0,0 @@
-import { getMongoRepository } from 'typeorm';
-
-
-
